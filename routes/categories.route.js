a913d579b259832ec63e9f8e90a0a52547fc0bea
lom-ali <lomali1999999@mail.ru>
2023-07-25 14:18:27 +0300

    изменил название категории

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -1,10 +1,10 @@
-const {categoriesController} = require('../controllers/categories.controller')
+const {categoryController} = require('../controllers/categories.controller')
 const {Router} = require('express')
 
 const router = Router()
 
-router.get('/categories', categoriesController.getCategories)
-router.post('/categories', categoriesController.createCategories)
-router.delete('/categories/:id', categoriesController.deleteCategories)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.createCategories)
+router.delete('/categories/:id', categoryController.deleteCategories)
 
 module.exports = router
