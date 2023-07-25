5bc8cafe610ca633ce72eb23fbb46a4e97cffdc3
lom-ali <lomali1999999@mail.ru>
2023-07-25 13:35:00 +0300

    добавил категории

---
+++ models/Categories.model.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const categoriesSchema = mongoose.Schema({
+    name: String
+})
+const Categories = mongoose.model('Categories', categoriesSchema)
+module.exports = Categories
