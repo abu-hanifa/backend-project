aa043238ea61f9c1188f327374e2463ba4e0dab8
lom-ali <lomali1999999@mail.ru>
2023-07-25 16:26:29 +0300

    subCategories

--- models/Category.model.js
+++ models/Category.model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 const categorySchema = mongoose.Schema({
-    name: String
+    name: String,
+    subcategories: String
 })
 const Category = mongoose.model('Category', categorySchema)
 module.exports = Category
