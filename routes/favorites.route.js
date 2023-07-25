cea4ebe59651445627ab44519540095856599e96
Sherlock0595 <magomed.umaev@mail.ru>
2023-07-25 15:14:19 +0300

    fix error

--- routes/favorites.route.js
+++ routes/favorites.route.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const { favoriteController } = require("../controllers/favorite.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 const router = Router();
-
 router.get('/favorites', authMiddleware,  favoriteController.getFavorite );
 router.patch('/favorites/:id',authMiddleware, favoriteController.addFavorite);
 router.delete('/favorites/:id', authMiddleware, favoriteController.deleteFavorite);
