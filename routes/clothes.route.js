0f991163e63cedf080a94bf23c46d23b0014f7d9
Rasvom <rasvomb@gmail.com>
2023-07-26 18:07:33 +0300

    add file middlewares

--- routes/clothes.route.js
+++ routes/clothes.route.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
-const { clothesController } = require('../controllers/clothes.controller')
+const { clothesController } = require("../controllers/clothes.controller");
+const fileMiddleware = require("../middlewares/file.middleware");
 const router = Router();
 
 router.get("/cloth", clothesController.getCloth); // вывод всей одежды
 // router.get("/cloth/category/:id", clothesController.); // вывод одежды по категории
-router.post("/cloth", clothesController.addCloth); // добавление одежды
+router.post(
+  "/cloth",
+  fileMiddleware.array("img", 4),
+  clothesController.addCloth
+); // добавление одежды
 
 module.exports = router;
